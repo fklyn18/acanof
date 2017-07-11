ce0a2e645b67c602114e4777fa7e0eada32f55b1
Franklyn <fklyn18@gmail.com>
2017-07-11 16:08:23 -0400

    tabla media

---
+++ database/migrations/2017_07_11_190246_create_media_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->integer('ididea');
+            $table->smallInteger('type')->comment('1:Logo, 2:Video, 3:Image');
+            $table->smallInteger('location')->comment('1:Local, 2:External');
+            $table->string('src', 1000)->comment('Media file path');
+            $table->timestamps();
+            $table->foreign('ididea')->references('id')->on('ideas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
