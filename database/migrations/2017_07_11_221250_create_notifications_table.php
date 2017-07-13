917bff3f862265fbd3e0e41ac64fbbbef5b21e50
Franklyn Cedeño <fklyn18@gmail.com>
2017-07-13 08:25:55 -0400

    Optimization from notifications table in relational to users

--- database/migrations/2017_07_11_221250_create_notifications_table.php
+++ database/migrations/2017_07_11_221250_create_notifications_table.php
@@ -14,9 +14,13 @@ class CreateNotificationsTable extends Migration
     public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
+            $table->integer('idusertransmitter')->comment('Transmitting user');
+            $table->integer('iduserreceiver')->comment('Receiving user');
             $table->integer('type')->comment('1:Tracking, 2:To define, etc...');
             $table->integer('idnotification')->comment('Id from notifications...');
             $table->timestamps();
+            $table->foreign('idusertransmitter')->references('id')->on('users');
+            $table->foreign('iduserreceiver')->references('id')->on('users');
         });
     }
 
